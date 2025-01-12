347aec4abe06c1fa032ed7cc26cef8c53075a39e
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-12 23:24:52 +0600

    Button component created

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({text,link}) => {
     return (
         <div>
-            
+            <button className=' p-2 text-white btn-outline outline-white'> <a href={link}> {text} </a> </button>
         </div>
     );
 };
