ff63dcc33b471d571a3169036b9276a2ec02bed6
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-13 00:00:45 +0600

    heading added to welcome section

---
+++ src/pages/Home/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome to
+            Property Development Limited.</h1>
+        </div>
+    );
+};
+
+export default Welcome;
