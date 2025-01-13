3f7970ff216f12e07cc20bc4a45cb54f4acdc921
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-13 22:52:58 +0600

    Construction added in array

--- src/pages/Home/Welcome.jsx
+++ src/pages/Home/Welcome.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Welcome = () => {
+    const services = [
+        {id:1,
+         service: 'Construction'
+        }
+    ]
     return (
         <div>
             <h1>Welcome to
            Build Better Limited.</h1>
             <p>A legacy of almost 4 decades, we stepped into manufacturing building products in hopes to deliver materials that will be a game-changer in the construction sector. Since 2014, our building products have earned a much-deserved reputation for being one of the best in the country for being safer, stronger, and more durable than others in the industry.</p>
+            <div className="flex gap-6 items-center"></div>
         </div>
     );
 };
