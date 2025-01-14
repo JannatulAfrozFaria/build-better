6968e53c62250cb5a749c11ec14e1c99f6247149
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-14 17:30:28 +0600

    service titles added to cards

--- src/pages/Home/Welcome.jsx
+++ src/pages/Home/Welcome.jsx
@@ -20,7 +20,13 @@ const Welcome = () => {
             <h1>Welcome to
            Build Better Limited.</h1>
             <p>A legacy of almost 4 decades, we stepped into manufacturing building products in hopes to deliver materials that will be a game-changer in the construction sector. Since 2014, our building products have earned a much-deserved reputation for being one of the best in the country for being safer, stronger, and more durable than others in the industry.</p>
-            <div className="flex gap-6 items-center"></div>
+            <div className="flex gap-6 items-center">
+                {services.map((item)=>(
+                    <div key={item.id}>
+                        <h2 className='font-medium'>{item.service} </h2>
+                    </div>
+                ) )}
+            </div>
         </div>
     );
 };
