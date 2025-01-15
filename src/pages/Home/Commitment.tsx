b1dee9dc44153798fd68fe7378aeeee19200db8c
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-15 23:10:07 +0600

    react-icons installed

--- src/pages/Home/Commitment.tsx
+++ src/pages/Home/Commitment.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Commitment = () => {
     return (
-        <div>
+        <div className='bg-[#e8e4c1]  p-16'>
             <h1>Committed to Excellence</h1>
+            <p>The significance of choosing the right products for construction is great. No compromise should be made in this case if we want a safe future. And we unfailingly work towards creating that safe future for you. With us, you will find:</p>
+            <div className='w-[70%] mx-auto '></div>
         </div>
     );
 };
