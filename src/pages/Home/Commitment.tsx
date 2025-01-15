05167b736609d3ce29d274ba9c5c56e6ca6154bc
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-15 23:28:31 +0600

    item rendered from excellence

--- src/pages/Home/Commitment.tsx
+++ src/pages/Home/Commitment.tsx
@@ -1,11 +1,25 @@
+import { title } from 'process';
 import React from 'react';
-
+import reliability from "../../../src/images/reliable.png";
 const Commitment = () => {
+    const excellences = [
+        {id:1,
+         title: 'Reliability',
+         icon: reliability
+        },
+    ]
     return (
         <div className='bg-[#e8e4c1]  p-16'>
             <h1>Committed to Excellence</h1>
             <p>The significance of choosing the right products for construction is great. No compromise should be made in this case if we want a safe future. And we unfailingly work towards creating that safe future for you. With us, you will find:</p>
-            <div className='w-[70%] mx-auto '></div>
+            <div className='w-[70%] mx-auto '>
+                {excellences.map((item)=> (
+                    <div key={item.id} className="flex gap-4 items-center">
+                        <div> <img src={item.icon} alt="" /> </div>
+                        <div> {item.title} </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
