8da8addc199d8466d694394ce3cab3595c4bbbad
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-17 23:56:50 +0600

    Benifits array created

--- src/pages/Home/Benifits.jsx
+++ src/pages/Home/Benifits.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const Benifits = () => {
-    return (
-        <div>
-            <h1>Benifits of Concrete Block</h1>
-        </div>
-    );
+  const Benifits = [
+    {
+      id: 1,
+      title: "Reduction inConstruction Cost",
+      decription:
+        "In the production of bricks, massive land and resources are wasted which consequently drives up construction cost. In such a case, manufacturing and utilizing concrete blocks significantly reduces cost and saves natural resources. Each block can save approximately 25% more natural resources than a brick.",
+    },
+    {
+      id: 1,
+      title: "Reduction inConstruction Cost",
+      decription:
+        "In the production of bricks, massive land and resources are wasted which consequently drives up construction cost. In such a case, manufacturing and utilizing concrete blocks significantly reduces cost and saves natural resources. Each block can save approximately 25% more natural resources than a brick.",
+    },
+    {
+      id: 3,
+      title: "Longevity and Durability",
+      decription:
+        "In the production of bricks, massive land and resources are wasted which consequently drives up construction cost. In such a case, manufacturing and utilizing concrete blocks significantly reduces cost and saves natural resources. Each block can save approximately 25% more natural resources than a brick.",
+    },
+    {
+      id: 4,
+      title: "Energy Saving Properties",
+      decription:
+        "In the production of bricks, massive land and resources are wasted which consequently drives up construction cost. In such a case, manufacturing and utilizing concrete blocks significantly reduces cost and saves natural resources. Each block can save approximately 25% more natural resources than a brick.",
+    },
+    {
+      id: 5,
+      title: "Design Flexibility",
+      decription:
+        "In the production of bricks, massive land and resources are wasted which consequently drives up construction cost. In such a case, manufacturing and utilizing concrete blocks significantly reduces cost and saves natural resources. Each block can save approximately 25% more natural resources than a brick.",
+    },
+  ];
+  return (
+    <div>
+      <h1>Benifits of Concrete Block</h1>
+    </div>
+  );
 };
 
 export default Benifits;
