708cc9d376cbe90559a56d8bde3caa3b1463e6b5
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-20 00:24:57 +0600

    item 3 added

--- src/pages/Home/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const WhyChooseUs = () => {
     const USP = [
         {id:1,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
-        {id:2,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
-        {id:3,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
+        {id:2,title: 'PDL enjoys constant and stable business and working relationships with its strategic partners for design, construction and equipment supply.' },
+        {id:3,title: 'Information Advantage', details: 'PDL’s business staff, spread all over the world, has extensive channels of information acquisition and the capacity for fast information feedback and processing.' },
         {id:4,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
     ]
     return (
