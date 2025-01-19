65060e990970cd8189dee8fe3b88de1ab8733d6f
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-20 00:23:57 +0600

    4 items added to array

--- src/pages/Home/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const WhyChooseUs = () => {
     const USP = [
-        {id:1,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' }
+        {id:1,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
+        {id:2,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
+        {id:3,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
+        {id:4,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
     ]
     return (
         <div className='bg-green-900 text-neutral-content'>
