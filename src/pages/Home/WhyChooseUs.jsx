e8f2f63cd90e8152d7483edd3c40e268eea7d6a3
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-01-20 23:49:26 +0600

    USP mapped

--- src/pages/Home/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const WhyChooseUs = () => {
     const USP = [
-        {id:1,title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
-        {id:2,title: 'PDL enjoys constant and stable business and working relationships with its strategic partners for design, construction and equipment supply.' },
-        {id:3,title: 'Information Advantage', details: 'PDL’s business staff, spread all over the world, has extensive channels of information acquisition and the capacity for fast information feedback and processing.' },
-        {id:4,title: 'Expert Team', details: 'PDL outstanding team is made up of young, ambitious, quality and capable staff numbers' },
+        {id:1, number:1, title: 'Financial Capability', details: 'As derived from its operations and activities involving foreign trade, economic and technology cooperation, PDL’s sterling reputation for enabling healthy economic profit and ensuring quality work has gained it access to credit, capital and loan support from numerous notable financial institutions.' },
+        {id:2, number:2, title: 'PDL enjoys constant and stable business and working relationships with its strategic partners for design, construction and equipment supply.' },
+        {id:3, number:3, title: 'Information Advantage', details: 'PDL’s business staff, spread all over the world, has extensive channels of information acquisition and the capacity for fast information feedback and processing.' },
+        {id:4, number:4, title: 'Expert Team', details: 'PDL outstanding team is made up of young, ambitious, quality and capable staff numbers' },
     ]
     return (
         <div className='bg-green-900 text-neutral-content'>
@@ -13,7 +13,13 @@ const WhyChooseUs = () => {
             <hr />
             <div className="grid grid-cols-2 gap-12">
                 {USP.map((item)=>(
-                    <div key={item.id} ></div>
+                    <div key={item.id} className='flex items-center gap-6' >
+                        <div> {item.number} </div>
+                        <div>
+                            <p> {item.title} </p>
+                            <p>{item.details} </p>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
